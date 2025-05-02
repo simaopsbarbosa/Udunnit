e15139a1489bcde1394d05c291f5f2a7984907b2
Simão Barbosa <simao.barbosa.05@gmail.com>
2025-05-02 11:43:15 +0100

    added pages

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Tasks Screen'),
+    );
+  }
+}
